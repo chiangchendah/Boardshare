d9a7efe7bd7f293bdfb79e8e959ac9b5c7d548ea
Tyler Travers <ttravers17@gmail.com>
2015-07-10 13:41:13 -0400

    setup custom event for templates

--- client/app/canvas/canvas.js
+++ client/app/canvas/canvas.js
@@ -1,6 +1,7 @@
 var makeResizableCanvas = require('../helpers/resizeElement').makeResizableCanvas;
 var setImmediate = require('../helpers/setImmediate');
+var elem = document.getElementById('paint-canvas');
 
-setImmediate(function(){
-  makeResizableCanvas(document.getElementById('canvas'));
+elem.addEventListener('templateRendered', function() {
+  console.log('canvas template rendered');
 });

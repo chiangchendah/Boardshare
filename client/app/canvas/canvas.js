3cd36a81b4d3fa526759275a72d9fedb52a0dcf3
Tyler Travers <ttravers17@gmail.com>
2015-07-14 11:18:34 -0400

    added fabric and changed handlebars template

--- client/app/canvas/canvas.js
+++ client/app/canvas/canvas.js
@@ -1,70 +1,110 @@
-var drawGrid = require('./canvasHelpers/drawGrid');
-var drawBackground = require('./canvasHelpers/drawBackground');
-var mouseActions = require('./canvasHelpers/mouseActions');
-var getCoords = require('./canvasHelpers/getCoords');
-var drawingSurface = require('./canvasHelpers/saveAndRestore');
-
+var fabric = require('fabric-browserify').fabric;
 
 module.exports = function() {
-  // Declare variables
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
-  var strokeStyleSelect = document.getElementById('strokeStyleSelect');
-  var fillStyleSelect = document.getElementById('fillStyleSelect');
-  var lineWidthSelect = document.getElementById('lineWidthSelect');
+  var canvas = new fabric.Canvas('canvas', {selection: false});
+  var dragging = false;
+
+  var backgroundColorSelect = document.getElementById('backgroundColor');
+  var strokeColorSelect = document.getElementById('strokeColor');
+  var fillColorSelect = document.getElementById('fillColor');
+  var lineWidthSelect = document.getElementById('lineWidth');
   var toolSelect = document.getElementById('toolSelect');
-  var eraseAllButton = document.getElementById('eraseAllButton');
-  var snapshotButton = document.getElementById('snapshotButton');
-  var undoButton = document.getElementById('undoButton');
+  var clearAllButton = document.getElementById('clearAll');
+  var undoButton = document.getElementById('undo');
+  var redoButton = document.getElementById('redo');
 
-  var selectedFunction = toolSelect.value || 'line';
-  var SHADOW_COLOR = 'rgba(0,0,0,0.7)';
+  var origX, origY;
+  var rect, line;
 
-  // Control Event Handlers
-  strokeStyleSelect.onchange = function(e) {
-    context.strokeStyle = strokeStyleSelect.value;
-  };
-  fillStyleSelect.onchange = function(e) {
-    context.fillStyle = fillStyleSelect.value;
-  };
-  lineWidthSelect.onchange = function(e) {
-    context.lineWidth = lineWidthSelect.value;
-  };
+  var selectedFunction = toolSelect.value;
+  canvas.isDrawingMode = true;
+
   toolSelect.onchange = function(e) {
-    selectedFunction = toolSelect.value;
+    if (this.value !== 'pencil') {
+      canvas.isDrawingMode = false;
+    } else {
+      canvas.isDrawingMode = true;
+    }
   };
-  eraseAllButton.onclick = function(e) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawGrid(context, 'black', 10, 10);
-    drawingSurface.save(canvas, context);
-  };
-  undoButton.onclick = function(e) {
-    // TODO
-  };
-
-  // Canvas Event Handlers
-  canvas.onmousedown = function(e) {
-    getCoords(e, function(loc) {
-      mouseActions.mouseDownInCanvas(loc, selectedFunction);
-    });
-  };
-  canvas.onmousemove = function(e) {
-    getCoords(e, function(loc) {
-      mouseActions.mouseMoveInCanvas(loc, selectedFunction);
-    });  
-  };
-  canvas.onmouseup = function(e) {
-    getCoords(e, function(loc) {
-      mouseActions.mouseUpInCanvas(loc, selectedFunction);
-    });
+  clearAllButton.onclick = function() {
+    canvas.clear();
   };
 
-  // Initialize
-  context.strokeStyle = strokeStyleSelect.value;
-  context.fillStyle = fillStyleSelect.value;
-  context.lineWidth = lineWidthSelect.value;
 
-  drawGrid(context, 'black', 10, 10);
-  drawBackground();
 };
+
+
+
+
+
+
+// var drawGrid = require('./canvasHelpers/drawGrid');
+// var drawBackground = require('./canvasHelpers/drawBackground').drawBackground;
+// var mouseActions = require('./canvasHelpers/mouseActions');
+// var getCoords = require('./canvasHelpers/getCoords');
+// var drawingSurface = require('./canvasHelpers/saveAndRestore');
+
+
+// module.exports = function() {
+//   // Declare variables
+//   var canvas = document.getElementById('canvas');
+//   var context = canvas.getContext('2d');
+//   var strokeStyleSelect = document.getElementById('strokeStyleSelect');
+//   var fillStyleSelect = document.getElementById('fillStyleSelect');
+//   var lineWidthSelect = document.getElementById('lineWidthSelect');
+//   var toolSelect = document.getElementById('toolSelect');
+//   var eraseAllButton = document.getElementById('eraseAllButton');
+//   var snapshotButton = document.getElementById('snapshotButton');
+//   var undoButton = document.getElementById('undoButton');
+
+//   var selectedFunction = toolSelect.value || 'line';
+//   var SHADOW_COLOR = 'rgba(0,0,0,0.7)';
+
+//   // Control Event Handlers
+//   strokeStyleSelect.onchange = function(e) {
+//     context.strokeStyle = strokeStyleSelect.value;
+//   };
+//   fillStyleSelect.onchange = function(e) {
+//     context.fillStyle = fillStyleSelect.value;
+//   };
+//   lineWidthSelect.onchange = function(e) {
+//     context.lineWidth = lineWidthSelect.value;
+//   };
+//   toolSelect.onchange = function(e) {
+//     selectedFunction = toolSelect.value;
+//   };
+//   eraseAllButton.onclick = function(e) {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     drawGrid(context, 'black', 10, 10);
+//     drawingSurface.save(canvas, context);
+//   };
+//   undoButton.onclick = function(e) {
+//     // TODO
+//   };
+
+//   // Canvas Event Handlers
+//   canvas.onmousedown = function(e) {
+//     getCoords(e, function(loc) {
+//       mouseActions.mouseDownInCanvas(loc, selectedFunction);
+//     });
+//   };
+//   canvas.onmousemove = function(e) {
+//     getCoords(e, function(loc) {
+//       mouseActions.mouseMoveInCanvas(loc, selectedFunction);
+//     });  
+//   };
+//   canvas.onmouseup = function(e) {
+//     getCoords(e, function(loc) {
+//       mouseActions.mouseUpInCanvas(loc, selectedFunction);
+//     });
+//   };
+
+//   // Initialize
+//   context.strokeStyle = strokeStyleSelect.value;
+//   context.fillStyle = fillStyleSelect.value;
+//   context.lineWidth = lineWidthSelect.value;
+
+//   drawGrid(context, 'black', 10, 10);
+//   drawBackground();
+// };
 
--- client/app/canvas/canvasHelpers/drawBackground.js
+++ client/app/canvas/canvasHelpers/drawBackground.js
@@ -1,9 +1,13 @@
 // This is an in-memory empty canvas. Used for erasing
 var drawGrid = require('./drawGrid');
-var backgroundContext = document.createElement('canvas').getContext('2d');
 
-module.exports = function() {
+function drawBackground() {
+  var backgroundContext = document.createElement('canvas').getContext('2d');
   backgroundContext.canvas.width = context.canvas.width;
   backgroundContext.canvas.height = context.canvas.height;
   drawGrid(backgroundContext, 'black', 10, 10);
+};
+
+module.exports = {
+  drawBackground: drawBackground
 };

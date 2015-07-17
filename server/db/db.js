7475500f0a5092ba3927b6dea771c6dd5c3228af
chiangchendah <chiangchendah@gmail.com>
2015-07-18 07:42:28 +0800

    update mongodb for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,24 @@
 var mongoose = require('mongoose');
 
-var mongoDB = process.env.MONGODB_URL || "mongodb://localhost/boardshare";
+var mongoDB = process.env.MONGOLAB_URI || "mongodb://localhost/boardshare";
 
 mongoose.connect(mongoDB);
 
 var UserSchema = new mongoose.Schema({
   githubId: { type: String, index: { unique: true } },
-  firepadId: String,
-  canvasJSON: String,
-  message: String
+  board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board' }
+});
+
+var BoardSchema = new mongoose.Schema({
+  canvas: String,
+  editor: String,
+  messages: String,
+});
+
+var MessagesSchema = new mongoose.Schema({
+  boardId: String,
+  text: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('User', UserSchema);

ccfbeeb35e670d66102fca155f66b7cf5e21e68d
chiangchendah <chiangchendah@gmail.com>
2015-07-18 10:23:42 +0800

    revise schema to have only users and boards

--- server/db/db.js
+++ server/db/db.js
@@ -6,19 +6,13 @@ mongoose.connect(mongoDB);
 
 var UserSchema = new mongoose.Schema({
   githubId: { type: String, index: { unique: true } },
-  board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board' }
+  board: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Board' }]
+  // use may have many boards
 });
 
 var BoardSchema = new mongoose.Schema({
   canvas: String,
-  editor: String,
-  messages: String,
-});
-
-var MessagesSchema = new mongoose.Schema({
-  boardId: String,
-  text: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  editor: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
